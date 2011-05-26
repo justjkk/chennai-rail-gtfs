302da17ba08b2f8b2ae40df292dc124045a3ab2b
Kishore kumar J <justjkk@gmail.com>
2011-05-27 05:10:28 +0530

    Converted MSB-TBM timetable from pdf to unpacked csv; Updated unpacking script

--- source/unpack.py
+++ source/unpack.py
@@ -17,17 +17,22 @@ def fmttime(uftime):
     '08:00:00'
     """
     timestr = str(uftime)
-    if ":" in timestr:
-        hour, minute = [int(x) for x in timestr.split(":")]
-    elif "." in timestr:
-        hour, minute = timestr.split(".")
-        hour = int(hour)
-        if len(minute) == 1:
-            minute = int(minute) * 10
+    try:
+        if ":" in timestr:
+            hour, minute = [int(x) for x in timestr.split(":")]
+        elif "." in timestr:
+            hour, minute = timestr.split(".")
+            hour = int(hour)
+            if len(minute) == 1:
+                minute = int(minute) * 10
+            else:
+                minute = int(minute)
         else:
-            minute = int(minute)
-    else:
-        hour, minute = (int(timestr), 0)
+            hour, minute = (int(timestr), 0)
+    except Exception, e:
+        sys.stderr.write("Unrecognised time format: '%s'\n" % timestr)
+        sys.stderr.write("Error was: '%s'\n" % e)
+        raise
     return "%02d:%02d:00" % (hour, minute)
 
 def unpack(filename):
@@ -36,6 +41,13 @@ def unpack(filename):
     input: service id, train number, stop time matrix...
     output: trip id, service id, trip short name, stop name, time, sequence
     """
+
+    STOP_COLUMNS_OFFSET = 2
+    with open(filename + '.csv', 'r') as f:
+        fr = csv.reader(f)
+        for row in fr:
+            stop_names = row[STOP_COLUMNS_OFFSET:]
+            break
 
     incsv = csv.DictReader(open(filename + '.csv', 'r'))
 
@@ -48,9 +60,11 @@ def unpack(filename):
     outcsv.writerow(dict([(x,x) for x in outcols])) # workaround for DictWriter.writeheader() function
 
     for trip in incsv:
-        sequence = 1
-        stop_names = list(set(trip.iterkeys()) - set(["Train Nos.", "Service Id"]))
-        for stop_name in sorted(stop_names, key=lambda x: fmttime(trip[x])):
+        sequence = 0
+        for stop_name in stop_names:
+            sequence += 1
+            if trip[stop_name] == None or trip[stop_name] == "":
+                continue
             outcsv.writerows([{
                 "trip_id":trip["Train Nos."] + trip["Service Id"],
                 "service_id":trip["Service Id"],
@@ -59,10 +73,13 @@ def unpack(filename):
                 "sequence": sequence,
                 "time": fmttime(trip[stop_name])
             }])
-            sequence += 1
 
 if __name__ == "__main__":
     unpack('mrts_sun_bvl')
     unpack('mrts_sun_vlb')
     unpack('mrts_wds_bvl')
     unpack('mrts_wds_vlb')
+    unpack('csr_sun_msb-tbm')
+    unpack('csr_sun_tbm-msb')
+    unpack('csr_wds_msb-tbm')
+    unpack('csr_wds_tbm-msb')
